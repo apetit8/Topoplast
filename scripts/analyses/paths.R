097f905d073d1407445f96d95a2ab11f876fb958
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2025-09-16 09:42:41 +0200

    Storage of paths in a specific R file.

---
+++ scripts/analyses/paths.R
@@ -0,0 +1,13 @@
+path <- list(
+    prog      = here::here("simevolv/bin/Release/Simul_Prog"),
+    param.dir = list(
+        fullnet = here::here("templates/Full_netw"),
+        drift   = here::here("templates/Full_netw_drift")),
+    sim.dir   = list(
+        fullnet = here::here("simul/Full_netw_75"), 
+        drift   = here::here("simul/Full_netw_drift")),
+    launch.file= list(
+        fullnet = here::here("launchers/Full_netw_75-launch.sh"),
+        drift   = here::here("launchers/Full_drift-launch.sh"))
+)
+
--- scripts/analyses/print_paramfiles.R
+++ scripts/analyses/print_paramfiles.R
@@ -1,23 +1,34 @@
 #!/usr/bin/env Rscript
-source("scripts/functions/functions.R")
-source("scripts/functions/makeparam_funct.R")
-################################################################################
-param.dir <- file.path("templates/Full_netw")
-directory <- "simul/Full_netw_75"
+source(here::here("scripts/analyses/paths.R"))
+source(here::here("scripts/functions/functions.R"))
+source(here::here("scripts/functions/makeparam_funct.R"))
+
+# This script generates the parameter files for the simulations. 
+
+# It consists in two parts that are almost identical; 
+# part 1 for the full network simulations
+# part 2 for the control (drift) simulations
+# note that path variables are overwritten between part 1 and part 2
+
+overwrite <- TRUE
+# options(warn=1)
+prog.path <- path$prog
+
+sd <- 0.25  #0.25
+min <- 0.15 #min environment
+max <- 0.85 #max environment
+
+####### FULL NETWORK SIMULATIONS ################
+
+param.dir      <- path$param.dir$fullnet
+directory      <- path$sim.dir$fullnet
+launchfilename <- path$launch.file$fullnet
+
 ifelse(!dir.exists(directory), dir.create(directory), FALSE)
 cache.dir <- normalizePath(file.path(directory))
 
-overwrite <- FALSE
-# options(warn=1)
-# prog.path <- "/shared/projects/evoplanet/Software/simevolv/bin/Release/Simul_Prog"
-prog.path <- "../../simevolv/bin/Release/Simul_Prog"
-#####
-sd <- 0.25 #0.25
-min <- 0.15#min environment
-max <- 0.85 #max environment
-#
-launchfilename <- "launchers/Full_netw_75-launch.sh"
-##########################
+
+####
 # Generate all simulations
 # A simulation is characterized by a name, a parameter file, and an extended parameter file (used to generate different environmental file each generations).
 # Simulations involve the creation of many small files (new parameters every generation due to plasticity). This is largely
@@ -38,26 +49,19 @@ for (sim.name in rownames(all.sims)) {
     file.path(cache.dir, sim.name), 
     overwrite=overwrite, verbose=FALSE, sd=sd, min=min, max=max)
   
-  create.launchfile.alt(prog.path, pars$param, pars$out, pars$compressed, relative.paths=TRUE,
+  create.launchfile.alt(prog.path, pars$param, pars$out, pars$compressed, relative.paths=FALSE,
                         oldpop="none", file.path(launchfilename), prevpop=FALSE, sampling="")
 }
 
-################################################################################
-param.dir <- file.path("templates/Full_netw_drift")
-directory <- "simul/Full_netw_drift"
+######### DRIFT SIMULATIONS #####################
+
+param.dir      <- path$param.dir$drift
+directory      <- path$sim.dir$drift
+launchfilename <- path$launch.file$drift
+
 ifelse(!dir.exists(directory), dir.create(directory), FALSE)
 cache.dir <- normalizePath(file.path(directory))
 
-overwrite <- FALSE
-# options(warn=1)
-prog.path <- "/shared/projects/evoplanet/Software/simevolv/bin/Release/Simul_Prog"
-# prog.path <- "../../simevolv/bin/Release/Simul_Prog"
-#####
-sd <- 0.25 #0.25
-min <- 0.15#min environment
-max <- 0.85 #max environment
-#
-launchfilename <- "launchers/Full_drift-launch.sh"
 ##########################
 # Generate all simulations
 # A simulation is characterized by a name, a parameter file, and an extended parameter file (used to generate different environmental file each generations).
@@ -65,6 +69,7 @@ launchfilename <- "launchers/Full_drift-launch.sh"
 # sub-optimal, but difficult to change given the way the simulation program works -- we have to deal with it. 
 
 #Delete previous launch file if exist
+
 ifelse(file.exists(launchfilename), unlink(launchfilename), FALSE)
 
 all.sims <- rbind(
@@ -78,8 +83,9 @@ for (sim.name in rownames(all.sims)) {
     file.path(param.dir, all.sims[sim.name, 2]), 
     file.path(cache.dir, sim.name), 
     overwrite=overwrite, verbose=FALSE, sd=sd, min=min, max=max)
+    
   
-  create.launchfile.alt(prog.path, pars$param, pars$out, pars$compressed, relative.paths=TRUE,
+  create.launchfile.alt(prog.path, pars$param, pars$out, pars$compressed, relative.paths=FALSE,
                         oldpop="none", file.path(launchfilename), prevpop=FALSE, sampling="")
 }
 
